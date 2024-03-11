0ab97f5877bfd69941f9e09418f38cf1c1f59fae
Xiaomin Liu <xl4624@nyu.edu>
2024-03-11 07:08:30 -0400

    Rewrite (1)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,106 +1,69 @@
-use std::{
-    fmt::{self, Display, Formatter},
-    iter::Peekable,
-    str::Chars,
-};
+use std::{collections::HashMap, error::Error, iter::Peekable, str::Chars};
 
-#[derive(Debug)]
-pub enum Token {
-    // Separators
-    LBrace,
-    RBrace,
-    LParen,
-    RParen,
-    Semi,
-    Equal,
+use crate::token::{Token, Type};
 
-    // Operators
-    Plus,
-    Minus,
-
-    // Keywords
-    Int,
-    Return,
-
-    Ident(String),
-
-    // Literals
-    IntLit(u32),
-}
-
-impl Display for Token {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Token::LBrace => write!(f, "l_brace '{{'"),
-            Token::RBrace => write!(f, "r_brace '}}'"),
-            Token::LParen => write!(f, "l_paren '('"),
-            Token::RParen => write!(f, "r_paren ')'"),
-            Token::Semi => write!(f, "semi ';'"),
-            Token::Equal => write!(f, "equal '='"),
-            Token::Plus => write!(f, "plus '+'"),
-            Token::Minus => write!(f, "minus '-'"),
-            Token::Int => write!(f, "int 'int'"),
-            Token::Return => write!(f, "return 'return'"),
-            Token::Ident(ident) => write!(f, "identifier '{}'", ident),
-            Token::IntLit(lit) => write!(f, "numeric_constant '{}'", lit),
-        }
-    }
-}
-
-pub fn lex(input: &str) -> Result<Vec<Token>, String> {
+pub fn lex(contents: String) -> Result<Vec<Token>, Box<dyn Error>> {
     let mut tokens = Vec::new();
-    let mut chars = input.chars().peekable();
+    let mut chars = contents.chars().peekable();
+    let keywords = keyword_token_map();
 
     while let Some(c) = chars.next() {
-        let token = match c {
-            '{' => Token::LBrace,
-            '}' => Token::RBrace,
+        tokens.push(match c {
             '(' => Token::LParen,
             ')' => Token::RParen,
+            '{' => Token::LBrace,
+            '}' => Token::RBrace,
             ';' => Token::Semi,
-            '=' => Token::Equal,
-            '+' => Token::Plus,
-            '-' => Token::Minus,
             _ if c.is_whitespace() => continue,
-            _ if c.is_alphabetic() => lex_word(&mut chars, c)?,
-            _ if c.is_numeric() => lex_number(&mut chars, c)?,
-            _ => return Err(format!("LEXER ERROR: Unexpected character {}", c)),
-        };
-        tokens.push(token);
+            _ if c.is_ascii_alphabetic() => lex_identifier_or_keyword(&mut chars, c, &keywords)?,
+            _ if c.is_numeric() => lex_number(&mut chars, c),
+            _ => return Err(format!("Unexpected character: {}", c).into()),
+        });
     }
+
     Ok(tokens)
 }
 
-fn lex_word(chars: &mut Peekable<Chars>, c: char) -> Result<Token, String> {
-    let mut identifier = c.to_string();
+fn lex_identifier_or_keyword(
+    chars: &mut Peekable<Chars>,
+    c: char,
+    keywords: &HashMap<String, Token>,
+) -> Result<Token, Box<dyn Error>> {
+    let mut word = c.to_string();
+
     while let Some(&next_c) = chars.peek() {
-        if next_c.is_ascii_alphanumeric() || next_c == '_' {
-            identifier.push(chars.next().unwrap());
-        } else {
+        if !(next_c.is_ascii_alphabetic() || next_c == '_') {
             break;
         }
+        // unwrap() is safe here because we know next_c is Some
+        word.push(chars.next().unwrap());
     }
-    let token = match identifier.as_str() {
-        "int" => Token::Int,
-        "return" => Token::Return,
-        _ => Token::Ident(identifier),
-    };
-    Ok(token)
+
+    match keywords.get(&word) {
+        Some(token) => Ok(token.clone()),
+        None => Ok(Token::Identifier(word)),
+    }
 }
 
-fn lex_number(chars: &mut Peekable<Chars>, c: char) -> Result<Token, String> {
-    let mut number_str = c.to_string();
-
+fn lex_number(chars: &mut Peekable<Chars>, c: char) -> Token {
+    let mut number = c.to_string();
     while let Some(&next_c) = chars.peek() {
-        if next_c.is_numeric() {
-            number_str.push(chars.next().unwrap());
-            chars.next();
-        } else {
+        if !next_c.is_numeric() {
             break;
         }
+        // unwrap() is safe here because we know next_c is Some
+        number.push(chars.next().unwrap());
     }
-    let number: u32 = number_str
-        .parse()
-        .map_err(|_| format!("LEXER ERROR: Failed to parse integer literal '{}'", number_str))?;
-    Ok(Token::IntLit(number))
+
+    Token::IntLit(number.parse().unwrap())
+}
+
+fn keyword_token_map() -> HashMap<String, Token> {
+    let mut keywords = HashMap::new();
+
+    keywords.insert("return".to_string(), Token::Return);
+    keywords.insert("int".to_string(), Token::Type(Type::Int));
+    keywords.insert("void".to_string(), Token::Type(Type::Void));
+
+    keywords
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-mod codegen;
+mod ast;
 mod lexer;
 mod parser;
+mod semantic_analyzer;
+mod token;
 
-use std::{env::args, error::Error, fs};
+use std::{error::Error, fs::read_to_string};
 
-use crate::{
-    codegen::generate_assembly,
-    lexer::{lex, Token},
-    parser::{parse, Program},
-};
+use crate::{lexer::lex, parser::parse, semantic_analyzer::analyze, token::Token};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = args().collect();
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: {} <filename>", args[0]);
+        eprintln!("Usage: {} <file>", args[0]);
         std::process::exit(1);
     }
+    let contents = read_to_string(&args[1])?;
 
-    let filename: &str = &args[1];
-    let input = std::fs::read_to_string(filename)?;
+    let tokens: Vec<Token> = lex(contents)?;
+    tokens.iter().for_each(|t| println!("{:?}", t));
 
-    let tokens: Vec<Token> = lex(&input)?;
-    tokens.iter().for_each(|t| println!("{}", t));
+    let ast = parse(&tokens)?;
+    println!("{:?}", ast);
 
-    let root: Program = parse(&tokens)?;
-    println!("Root: {:?}", root);
-
-    let assembly = generate_assembly(&root)?;
-    println!("{}", assembly);
-
-    let output_filename = format!("{}.s", filename.trim_end_matches(".c"));
-    fs::write(output_filename, assembly)?;
+    analyze(ast)?; // Check for semantic errors (undeclared variables or type mismatches)
 
     Ok(())
 }
